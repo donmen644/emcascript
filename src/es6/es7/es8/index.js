fd2a84101ca94da4161d100b6f4f3c3c6a20fce5
Mr Darkes <63811789+donmen644@users.noreply.github.com>
2021-02-20 12:25:40 -0600

    es8

--- src/es6/es7/es8/index.js
+++ src/es6/es7/es8/index.js
@@ -1,7 +1,7 @@
 const data = {
-frontend: 'Oscar',
-backend: 'Isabel',
-design: 'Ana',
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana',
 }
 
 const entries = Object.entries(data);
@@ -12,14 +12,38 @@ const data = {
     frontend: 'Oscar',
     backend: 'Isabel',
     design: 'Ana',
-    }
+}
 
 const values = Object.values(data);
 console.log(values)
 console.log(values.length)
 
 const string = 'hello';
-console.log(string.padStart(7,'hi'));
-console.log(string.padEnd(12,' ------'))
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' ------'))
 console.log('food'.padEnd(12, '  ------'))
 
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+const antoherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+antoherFunction();
