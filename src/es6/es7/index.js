81e9e31778f3fef6eff456813b4537c1577a390a
Mr Darkes <63811789+donmen644@users.noreply.github.com>
2021-02-20 11:57:32 -0600

    por un dia mas sin errores

---
+++ src/es6/es7/index.js
@@ -0,0 +1,13 @@
+let numerbers = [1,2,3,4,5,8,9];
+
+if (numerbers.includes(6)){
+    console.log('Si encuentra el valor ');
+}else{
+ console.log('No se encuentra.')
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
